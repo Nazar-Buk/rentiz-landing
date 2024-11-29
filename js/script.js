55bf7fa41b09242311f562795eacf74b462503cc
Nazar Buk <nazarbuk27@gmail.com>
2024-11-30 01:59:41 +0200

    🧱 Add Hero section and responsive
    🎨 Add styles for Hero section.
    🧱 Add Hero section.
    📸 Add fav-icon.
    📸 Add decore icons.
    📸 Add Featured images.
    🧱 Add Featured section.
    🧪 Add script for Filter in Hero section.
    📱💄 Add mobile responsive for Hero section.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
 const menu = document.querySelector(".menu"); // Декстопне меню
 const menuBtn = document.querySelector(".menu-icon"); // іконка бургера (обгортка рисочок)
 const body = document.body;
+const filter = document.querySelector(".filter");
 
-console.log(menu, "menu");
-console.log(menuBtn, "menuBtn");
-console.log(body, "body");
+// console.log(filter, "filter"); // Глянь що тут!!! Тут є дочірні елементи
+// console.log(menu, "menu");
 
 if (menu && menuBtn) {
   menuBtn.addEventListener("click", () => {
     menu.classList.toggle("_active"); // до десктопного меню буде додаватися або забиратися клас active
     menuBtn.classList.toggle("_active");
     body.classList.toggle("_lock");
+  });
+}
+
+if (filter) {
+  const items = filter.querySelectorAll(".block-filter");
+
+  items.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      item.querySelector(".block-filter__dropdown").classList.toggle("_active");
+      item.querySelector(".block-filter__icon").classList.toggle("_active");
+
+      //   console.log(event, "event");
+      //   console.log(event.target, "event.target");
+
+      // event.target --> це сам елемент
+      // const filter = document.querySelector(".filter"); --> теж знайде event.target
+      // item.querySelector(".block-filter__icon")  --> теж знайде event.target
+      // НУ Я ТУТ РІЗНІ ЕЛЕМЕНТИ ШУКАВ ^
+
+      if (event.target.classList.contains("block-filter__item")) {
+        item.querySelector(".block-filter__value").textContent =
+          event.target.textContent;
+      }
+    });
   });
 }
